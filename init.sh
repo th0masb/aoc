b0d2cfda37fce1d3d7cc22a945b30e56066b9201
Thomas Ball <tomoliverball@gmail.com>
2023-12-01 16:16:19 +0000

    Add init script for new problems

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+problem_dir="$(dirname "$0")/$1/$2"
+
+mkdir -p "$problem_dir"
+
+touch "$problem_dir/input.txt"
+cat > "$problem_dir/solution.py" << EOL
+import sys
+
+with open(sys.argv[1]) as f:
+    input = f.readlines()
+
+EOL
